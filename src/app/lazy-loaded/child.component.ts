90c0c7700b1e03b21fe7bab15f019e2ab7cbccbc
Stephen Done <sdone@simplifile.com>
2016-12-16 09:29:18 -0700

    adding optional route params

--- src/app/lazy-loaded/child.component.ts
+++ src/app/lazy-loaded/child.component.ts
@@ -4,28 +4,33 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
     template: `
         <h3>Child {{childId}}</h3>
+        <h4>Last Child Viewed: Child {{lastChildId}}</h4>
         <button (click)="goToPreviousChild()">Previous Child</button>
         <button (click)="goToNextChild()">Next Child</button>
     `
 })
 export class ChildComponent implements OnInit {
     childId: number;
+    lastChildId: number;
 
     constructor (private router: Router, private activatedRoute: ActivatedRoute) {}
 
     ngOnInit() {
         this.activatedRoute.params
-            .subscribe((params: Params) => this.childId = +params['childId']);
+            .subscribe((params: Params) => {
+                this.childId = +params['childId'];
+                this.lastChildId = +params['lastChildId'];
+            });
     }
 
     goToPreviousChild() {
         let previousChildId = this.childId - 1;
-        this.router.navigate(['/lazy-loaded/child', previousChildId]);
+        this.router.navigate(['/lazy-loaded/child', previousChildId, {lastChildId: this.childId}]);
     }
 
     goToNextChild() {
         let nextChildId = this.childId + 1;
-        this.router.navigate(['/lazy-loaded/child', nextChildId]);
+        this.router.navigate(['/lazy-loaded/child', nextChildId, {lastChildId: this.childId}]);
     }
 
 }
